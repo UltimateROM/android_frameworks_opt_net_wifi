f4533e012125cbf5b621df9210183a99f9699c49
zhangshuxiao <zhangshuxiao@xiaomi.com>
2016-02-26 18:59:10 +0800

    When wificonfiguration is null, return a failure message to the caller.
    When call syncEnableNetwork, the netId may be incorrect, so
    the config of this netid is null. In this case, we need to
    return a failue message to the caller.
    
    Change-Id: I5a5a9e856142f09447aa52aa6603fa79acb3e0d1
    Signed-off-by: zhangshuxiao <zhangshuxiao@xiaomi.com>

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -7606,6 +7606,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                     if (config == null) {
                         loge("No network with id = " + netId);
                         messageHandlingStatus = MESSAGE_HANDLING_STATUS_FAIL;
+                        replyToMessage(message, message.what, FAILURE);
                         break;
                     }
 
